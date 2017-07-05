8ce5573da3ae34b22ba13137badf3d82bfc89ca8
Christian Hochlin <christian.hochlin@creuna.no>
2017-07-05 12:57:40 +0200

    Initial Commit

---
+++ Pucky.EpiserverAnalysis/Pucky.EpiserverAnalysis/CodeFixProvider.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Pucky.EpiserverAnalysis
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(PuckyEpiserverAnalysisCodeFixProvider)), Shared]
+    public class PuckyEpiserverAnalysisCodeFixProvider : CodeFixProvider
+    {
+        private const string title = "Add [CultureSpecific]";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(PuckyEpiserverAnalysisAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var ancestors = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf();
+            var declaration = ancestors.OfType<PropertyDeclarationSyntax>().First();
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: title,
+                    createChangedSolution: c => UsingCultureSpecificAttributeAsync(context.Document, declaration, c),
+                    equivalenceKey: title),
+                diagnostic);
+        }
+
+        async Task<Solution> UsingCultureSpecificAttributeAsync(Document document, PropertyDeclarationSyntax propertyDeclaration, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+
+            root = AddCultureSpecificAttribute(root, propertyDeclaration);
+
+            root = AddUsingStatementIfNecessary(root);
+
+            return document.WithSyntaxRoot(root).Project.Solution;
+            
+        }
+
+        SyntaxNode AddCultureSpecificAttribute(SyntaxNode root, PropertyDeclarationSyntax propertyDeclaration)
+        {
+            var identifierToken = propertyDeclaration.Identifier;
+            var newName = identifierToken.Text.ToUpperInvariant();
+            var attributes = propertyDeclaration.AttributeLists.Add(
+                SyntaxFactory.AttributeList(SyntaxFactory.SingletonSeparatedList(
+                    SyntaxFactory.Attribute(SyntaxFactory.IdentifierName("CultureSpecific"))
+                )));
+
+            return root.ReplaceNode(
+                    propertyDeclaration,
+                    propertyDeclaration.WithAttributeLists(attributes));
+        }
+
+        SyntaxNode AddUsingStatementIfNecessary(SyntaxNode root)
+        {
+            var compilation =
+            root as CompilationUnitSyntax;
+
+            var EpiserverAnnotationsUsingName =
+            SyntaxFactory.QualifiedName(
+                SyntaxFactory.IdentifierName("EPiServer"),
+                SyntaxFactory.IdentifierName("DataAnnotations"));
+
+            if (!AnnotationUsingStatementExists(compilation))
+            {
+                return root.InsertNodesAfter(compilation.Usings.Last(), new[]{
+                    SyntaxFactory.UsingDirective(
+                            EpiserverAnnotationsUsingName)
+                });
+            }
+
+            return root;
+        }
+
+        bool AnnotationUsingStatementExists(CompilationUnitSyntax compilation)
+            => compilation.Usings.Any(u => u.Name.GetText().ToString() == "EPiServer.DataAnnotations");
+    }
+}
---
+++ Pucky.EpiserverAnalysis/Pucky.EpiserverAnalysis/DiagnosticAnalyzer.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Pucky.EpiserverAnalysis
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PuckyEpiserverAnalysisAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "PuckyEpiserverAnalysis";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Property);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var property = (IPropertySymbol)context.Symbol;
+            if (!property.IsVirtual)
+                return;
+
+            var type = property.Type;
+            if (type.SpecialType != SpecialType.System_String)
+                return;
+
+            var attributes = property.GetAttributes();
+            if (attributes.Length < 1)
+                return;
+
+            if (!HasDisplayAttribute(attributes))
+                return;
+
+            if (HasCultureSpecificAttribute(attributes))
+                return;
+           
+            var diagnostic = Diagnostic.Create(Rule, property.Locations[0], property.Name);
+
+            context.ReportDiagnostic(diagnostic);
+        }
+
+        static bool HasDisplayAttribute(ImmutableArray<AttributeData> attributes)
+            => attributes.Any(i => i.AttributeClass.ToString() == "System.ComponentModel.DataAnnotations.DisplayAttribute");
+
+        static bool HasCultureSpecificAttribute(ImmutableArray<AttributeData> attributes)
+            => attributes.Any(i => i.AttributeClass.ToString() == "EPiServer.DataAnnotations.CultureSpecificAttribute");
+    }
+}
